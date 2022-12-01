47b94636eef2a6858ca5b4dbfd15f0e253e7b78a
Jandro <Jandro@10.188.36.65>
2022-12-01 11:41:57 +0100

    Commit version inicial

---
+++ pruebaGIT/src/pruebaGIT/prueba1.java
@@ -0,0 +1,13 @@
+package pruebaGIT;
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo GIT. Viva Linus Torwald");
+		
+		
+	}
+
+}
