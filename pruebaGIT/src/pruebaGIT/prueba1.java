dac8c8c4b00963e43e52c0337aee28794005e499
Jandro <Jandro@10.188.36.5>
2022-12-12 13:28:43 +0100

    Commit prueba GITHUB

--- pruebaGIT/src/pruebaGIT/prueba1.java
+++ pruebaGIT/src/pruebaGIT/prueba1.java
@@ -3,7 +3,8 @@ package pruebaGIT;
 public class prueba1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Esto está comentado
 
 		System.out.println("Hola mundo GIT. Viva Linus Torwald.");
 		System.out.println("Mario callate ya joder");
