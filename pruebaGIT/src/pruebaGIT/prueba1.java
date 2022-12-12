0e882d05e748502671fc935c608e2a8840087fcc
Jandro <Jandro@10.188.36.5>
2022-12-12 13:19:46 +0100

    Comentado

--- pruebaGIT/src/pruebaGIT/prueba1.java
+++ pruebaGIT/src/pruebaGIT/prueba1.java
@@ -5,7 +5,7 @@ public class prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola mundo GIT. Viva Linus Torwald");
+		System.out.println("Hola mundo GIT. Viva Linus Torwald.");
 		System.out.println("Mario callate ya joder");
 		System.out.println("Un genio el Rallo");
 		System.out.println("Hola Juanjo :)");
