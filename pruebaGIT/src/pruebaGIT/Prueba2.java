f934b34ddf56fb711f88062b097145a9b5f031c0
Jandro <Jandro@10.188.36.66>
2022-12-19 12:12:55 +0100

    PruebaGIT

---
+++ pruebaGIT/src/pruebaGIT/Prueba2.java
@@ -0,0 +1,11 @@
+package pruebaGIT;
+
+public class Prueba2 {
+	
+	private int numprueba;
+	private String nombreprueba;
+	
+	public void ejecutarPrueba() {
+		System.out.println("Hola Xuanxe");
+	}
+}
---
+++ pruebaGIT/src/pruebaGIT/prueba1.java
@@ -0,0 +1,17 @@
+package pruebaGIT;
+
+public class prueba1 {
+
+	public static void main(String[] args) {
+		
+		// Esto está comentado
+
+		System.out.println("Hola mundo GIT. Viva Linus Torwald.");
+		System.out.println("Mario callate ya joder");
+		System.out.println("Un genio el Rallo");
+		System.out.println("Hola Juanjo :)");
+		
+		//ESTOS CAMBIOS SOLO ESTÁN EN LA RAMA DEL BECARIO
+	}
+
+}
